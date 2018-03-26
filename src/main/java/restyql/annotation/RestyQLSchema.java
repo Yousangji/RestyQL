fc871d593a7e22b4fa78786c757658e735c74df6
Sangji Yoo <sangji.yoo@kono.ai>
2018-03-26 16:42:42 +0900

    rename project

--- src/main/java/restyql/annotation/RestyQLSchema.java
+++
@@ -1,9 +0,0 @@
-package restyql.annotation;
-
-import java.lang.annotation.*;
-
-@Target({ElementType.PARAMETER})
-@Retention(RetentionPolicy.RUNTIME)
-@Documented
-public @interface RestyQLSchema {
-}
